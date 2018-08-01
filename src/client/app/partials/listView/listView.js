d63c96c269fc96cad0339920cfa37f15c47a8157
josefbutts <buttsjosef@gmail.com>
2018-08-01 15:55:29 -0500

    basic gameVIew template built out, no data on page yet

---
+++ src/client/app/partials/listView/listView.js
@@ -0,0 +1,10 @@
+
+  
+  angular.module('listView', ['pickUp.services'])
+  .controller('TimeSelectController', function($scope, $location, GameReq, sharedProps) {
+     
+      $scope.data = {
+        model: null,
+        
+      };
+    });

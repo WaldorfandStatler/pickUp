3e61e2962c8595a99b60b35a685699d3473c9908
josefbutts <buttsjosef@gmail.com>
2018-08-01 15:40:02 -0500

    And thus it begins, the componentless wonder

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -13,5 +13,17 @@ angular.module('pickUp', ['ui.router', 'gameReqForm', 'games'])
       url: '/games',
       templateUrl: 'app/partials/games/games.html',
       controller: 'GamesController'
-    });
+    })//;
+    //Thus begins the the fun
+    .state('mapView', {
+      url: '/mapView',
+      templateUrl: 'app/partials/mapView/mapView.html',
+      controller: 'MapViewController'
+    })
+    .state('listView', {
+      url: '/listView',
+      templateUrl: 'app/partials/listView/listView.html',
+      controller: 'listViewController'
+    })
+
 });

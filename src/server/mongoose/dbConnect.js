922d2a977e90bb3078757e495f03f3c0a72be5c1
Mark Stark <markstarkart@gmail.com>
2018-07-31 19:24:22 -0500

    app starting in with npm run dev

--- src/server/mongoose/dbConnect.js
+++ src/server/mongoose/dbConnect.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGO_URI);
+import { MONGO_URI } from '../../../config';
+
+// mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(MONGO_URI);
 const db = mongoose.connection;
 
-console.log('MONGO_URI: ', process.env.MONGO_URI);
-console.log('PORT: ', process.env.PORT);
+// console.log('MONGO_URI: ', process.env.MONGO_URI);
+// console.log('PORT: ', process.env.PORT);
+
+console.log('MONGO_URI: ', MONGO_URI);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('And we\'re in!!!');

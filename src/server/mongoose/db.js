eb149c74683cccef243b414c890d9ebb06e5024b
josefbutts <buttsjosef@gmail.com>
2018-08-01 17:07:51 -0500

    getAllGames db function started

--- src/server/mongoose/db.js
+++ src/server/mongoose/db.js
@@ -19,6 +19,15 @@ const db = {
         resolve(game);
       });
     });
+  },
+  //this is probably janked up and broken
+  getAllGames: () => {
+    return new Promise((resolve, reject) => {
+      Game.find().all({}, (err, game) => {
+        if (err) return reject(err);
+        resolve(game);
+      });
+    });
   }
 };
 

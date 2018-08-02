6d5cfe1a0fe64f7cdf278bdaf6f1cb2d92d11415
josefbutts <buttsjosef@gmail.com>
2018-08-02 00:51:42 -0500

    yep

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -13,25 +13,40 @@ angular.module('pickUp', ['ui.router', 'gameReqForm', 'games'])
         url: '/games',
         templateUrl: 'app/partials/games/games.html',
         controller: 'GamesController'
-      }) //;
+      })
+      .state('listView', {
+        url: '/listView',
+
+        // templateUrl: 'app/partials/listView/listView.html',
+        controller: 'ListViewController',
+        // data : 'soccer',
+        resolve: {
+          games: function (GetGames) {
+            GetGames.getAllGames()
+            .then( (result)=>{
+              console.log('result from app.js', result);
+              return result;
+              
+            });
+            
+            console.log('games', games);
+            
+          }
+        } //;
+      })
       //Thus begins the fun
       .state('mapView', {
         url: '/mapView',
         templateUrl: 'app/partials/mapView/mapView.html',
         controller: 'MapViewController'
       })
-      .state('listView', {
-        url: '/listView',
-        templateUrl: 'app/partials/listView/listView.html',
-        controller: 'ListViewController',
-        // data : 'soccer',
-        resolve: {
-          games: function (GetGames) {
-          console.log(GetGames.getAllGames());
-          return GetGames.getAllGames();
-          }
-        }
+      
+       
+        
         
       })
+      // app.controller('ListViewController', function($scope, games){
+      //   $scope.games = games;
+      // })
 
-  });
+  
--- src/client/app/partials/listView/listView.js
+++ src/client/app/partials/listView/listView.js
@@ -1,16 +1,20 @@
 
-  
   angular.module('listView', ['pickUp.services'])
-  .controller('ListViewController', function($scope,  GetGames, sharedProps) {
-    console.log('list view controller');
-      console.log("games", games);
-      $scope.games = games.data.$$state.value.data;
-      console.log($scope);
-    //   $scope.games = GetGames.getAllGames();
-    //   {
-    //     GetGames.getAllGames()
-    //     .then
-    //   };
+  // .controller('ListViewController', function( $scope,  GetGames, sharedProps) {
+  //     console.log('list view controller', $scope);
+  //     $scope.games = games;
+  //     console.log("resolve.games", resolve.games);
+  //     console.log($scope);
+    // //   $scope.games = GetGames.getAllGames();
+    // //   {
+    // //     GetGames.getAllGames()
+    // //     .then
+    // //   };
+
+    // $scope.data = {
+    //   model: null,
+     
+    // };
 
     
-    });
+    // });

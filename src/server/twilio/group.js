edcc7d5d7954dc550e834696b365ea1bcfc182f0
Mark Stark <markstarkart@gmail.com>
2018-08-03 10:20:54 -0500

    echoing back to sender

--- src/server/twilio/group.js
+++ src/server/twilio/group.js
@@ -6,11 +6,11 @@ const group = {
   handleSms: (req, res) => {
     console.log(req.body);
     
-    var twilio = require('twilio');
+    // var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
     twiml.message(function () {
-      this.body('The Robots are coming! Head for the hills!');
-      this.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
+      this.body(req.body.Body);
+      // this.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
     });
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());

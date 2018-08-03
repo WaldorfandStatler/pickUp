72d4392decc4e8920eaea66653e052deef56e971
Mark Stark <markstarkart@gmail.com>
2018-08-02 23:00:19 -0500

    changed text

--- src/server/twilio/sms.js
+++ src/server/twilio/sms.js
@@ -5,7 +5,7 @@ import { TWILIO_MSG_SID, TWILIO_AUTH_TOKEN, TWILIO_SID } from '../../../config';
 const client = twilio(process.env.TWILIO_SID || TWILIO_SID, process.env.TWILIO_AUTH_TOKEN || TWILIO_AUTH_TOKEN);
 const sms = {
   sendScheduledGame: ({smsNum, gameLoc, gameTime, sport}) => {
-    let message = `we're playing ${sport} @ ${gameLoc} for ${moment(gameTime).format('llll')}. You in?`;
+    let message = `${sport} is happening @ ${gameLoc} for ${moment(gameTime).format('llll')}. You in?`;
     console.log('sending message: ', message);
     // real text
     return new Promise((resolve, reject) => {

13f456d1bcec7b6a0e8f6ec6c8a621fea4577296
josefbutts <buttsjosef@gmail.com>
2018-08-02 23:38:31 -0500

    trying stuff

--- src/client/app/partials/listViewEntry/listViewEntry.js
+++ src/client/app/partials/listViewEntry/listViewEntry.js
@@ -1,6 +1,10 @@
 angular.module('listViewEntry', ['pickUp.services'])
   .controller('ListViewEntryController', function( $scope, GetGames, GameReq) {
-     console.log($scope);
+     
+    $scope.game = games;
+    console.log($scope, "from entry view");
+    console.log("scope from listviewentry", $scope.games);
+    
 
     let gameReq = {}
     $scope.requestGame = function() {
@@ -20,10 +24,21 @@ angular.module('listViewEntry', ['pickUp.services'])
       //     console.error('error requesting game ', error);
       //   });
         
-    };
+    }
+
     $scope.data = {
       model: null,
       // availableOptions: timeSlots,
       // selectedOption: timeSlots[0]
     }
+
   });
+  angular.module('pickUp')
+  .component('videoListEntry', {
+    bindings: {
+      games: '<'
+    },
+    templateUrl: 'src/templates/videoListEntry.html'
+  });
+
+
